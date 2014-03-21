5ec55069f8a9d62a6411ec9d61c8dd28a3a9cd71
alitams <ali.tams@gmail.com>
2014-03-21 22:58:22 +0100

    Main edit

--- src/tedteam/twotowers/main/Main.java
+++ src/tedteam/twotowers/main/Main.java
@@ -8,10 +8,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello TED team!");
 
-		int i = 10;
-		System.out.println("Változtatás "+i);
-		
-		System.out.println("Változtatás másik accounttal? Megy??");
 	}
 
 }
