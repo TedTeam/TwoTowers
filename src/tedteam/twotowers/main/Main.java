2bbdfc24227734441f763396ad8f5a058cc27bc3
TedTeam <ted.team10@gmail.com>
2014-03-21 21:29:51 +0100

    Változtatás2

---
+++ src/tedteam/twotowers/main/Main.java
@@ -0,0 +1,15 @@
+package tedteam.twotowers.main;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello TED team!");
+
+		int i = 10;
+		System.out.println("Változtatás "+i);
+	}
+
+}
