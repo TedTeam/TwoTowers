c4839646f5cdd8372e6cfc8514fbb65c549ad609
ZsoltKovari <kovari.zsolt91@gmail.com>
2014-03-23 13:00:22 +0100

    Enhance+setSlowing visszatérési érték módosítás+Komment

--- src/tedteam/twotowers/main/Blocker.java
+++ src/tedteam/twotowers/main/Blocker.java
@@ -1,25 +1,38 @@
 package tedteam.twotowers.main;
 
 import tedteam.twotowers.logger.Logger;
-
+/**
+ * Az akadalyt megvalosito osztaly, amely az akadaly
+ * tulajdonsagait tartalmazza.
+ * Ososztaly: Element
+ */
 public class Blocker extends Element {
-
-	private GameState gameState = new GameState();
+	// Ezen a GameState referencian keresztul tudja majd torolni
+	// magat a listabol, ha eleg ellenseg haladt at rajta.
+	private GameState gameState= new GameState();
+	// A feltarthato ellensegek szama.
 	private int countRemain = 0;
+	// Az akadaly lassitasanak merteke.
 	private int slowing = 0;
 	
 	/**
-	 * 
-	 * @param m
+	 * Ez a metodus donti el, hogy a varazskovel lehet-e erositeni
+	 * az akadalyt. Ha igen, akkor erositi is.
+	 * @param blackStone: a varazsko, mellyel erositeni szeretnenk az akadalyt.
+	 * @return visszajelzes, hogy sikeres volt-e a muvelet.
 	 */
 	public boolean enhance(MagicStone blackStone) {
-		Logger.enter("blocker", "enchance", "blackStone", "");
-		blackStone.effect(this);
-		
+		Logger.enter("blocker", "enhance", "blackStone", "");
+		//blackStone.effect(this);
+		boolean effect=blackStone.effect(this);
 		Logger.exit("true");
-		return true;
+		return effect;
 	}
 
+	/**
+	 * Beallitja magat egy cellaba.
+	 * @param cell: ebbe a cellaba allitja be magat.
+	 */
 	public void setCell(Cell cell) {
 		Logger.enter("blocker", "setCell", "", "");
 		
@@ -28,28 +41,40 @@ public class Blocker extends Element {
 		Logger.exit("void");
 	}
 
+	/**
+	 * Beallitja a GameState attributumot
+	 * @param gameState: ezt allitja be a gameState attributumba
+	 */
 	public void setGameState(GameState gameState) {
 		Logger.enter("blocker", "setGameState", "gameState","");
 		this.gameState = gameState;
 		
 		Logger.exit("void");
 	}
-	
+	/**
+	 * Modositja az akadaly lassitasanak merteket
+	 * @param i: a modositas merteke
+	 */
 	public void setSlowing(int i) {
 		Logger.enter("blocker", "setSlowing", "i:int", "");
 		
-		Logger.exit("true");
+		Logger.exit("void");
 	}
 	
+	/**
+	 * A blokkolast vegrehajto metodus.
+	 */
 	public void action() {
 		Logger.enter("blocker", "action", "", "");
-		
+		int slowing=0;
 		cell.getEnemy();
+		Enemy elf=new Elf();
+		elf.block(slowing);
 		Logger.exit("void");	
 	}
 	
 	/**
-	 * Loggoláshoz, skeleton után törlendő!
+	 * Loggolashoz, skeleton után torlendo!
 	 * @return
 	 */
 	public String loggerGetName() {
