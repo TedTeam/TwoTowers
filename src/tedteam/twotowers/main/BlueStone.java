e2533896137dc2f468bbeca1815de26e6efaf4d4
ZsoltKovari <kovari.zsolt91@gmail.com>
2014-03-23 13:01:42 +0100

    Effect return(Blocker)=true + Komment

--- src/tedteam/twotowers/main/BlueStone.java
+++ src/tedteam/twotowers/main/BlueStone.java
@@ -1,18 +1,24 @@
 package tedteam.twotowers.main;
-
+/**
+ * A kek varazskovet megvalosito osztaly.
+ * Ososztaly: MagicStone
+ */
 public class BlueStone extends MagicStone {
 
 	/**
-	 * 
-	 * @param b
+	 * Ez a metodus valtoztatja meg az akadaly hatotavolsaganak nagysagat.
+	 * Mivel ilyen attributuma nincs az akadalynak, ezert ez a metodus nem csinal semmit.
+	 * @param b: az akadaly, melyre a valtoztatast alkalmazni akarjuk.
+	 * @return a visszateresi ertek, hogy sikerult-e rarakni a kovet az akadalyra (false).
 	 */
 	public boolean effect(Blocker b){
-		return false;
+		return true;
 	}
 
 	/**
-	 * 
-	 * @param t
+	 * Ez a metodus valtoztatja meg a torony hatotavolsaganak nagysagat.
+	 * @param t: a torony, melyre a valtoztatast alkalmazni akarjuk.
+	 * @return a visszateresi ertek, hogy sikerult-e rarakni a kovet a toronyra.
 	 */
 	public boolean effect(Tower t){
 		return false;
