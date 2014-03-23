d1d7a03ca70065d3527cd012bac3633ceddfab22
ZsoltKovari <kovari.zsolt91@gmail.com>
2014-03-23 13:14:28 +0100

    Step módosítás formercell törlésre + Komment

--- src/tedteam/twotowers/main/Hobbit.java
+++ src/tedteam/twotowers/main/Hobbit.java
@@ -1,12 +1,15 @@
 package tedteam.twotowers.main;
 
 import tedteam.twotowers.logger.Logger;
-
+/**
+ * A hobbit ellenseget megvalosito osztaly.
+ * Ososztaly: Enemy
+ */
 public class Hobbit extends Enemy {
 
 	/**
-	 * 
-	 * @param d
+	 * Ezen a metoduson keresztul valtozik az ellenseg eletereje.
+	 * @param d: a valtozas merteke.
 	 */
 	public void acceptDamage(IDamage d){
 		Logger.enter("hobbit", "acceptDamage", "tower", "");
@@ -18,9 +21,9 @@ public class Hobbit extends Enemy {
 	}
 
 	/**
-	 * 
-	 * @param g
-	 * @param c
+	 * Beallitja az ellenseg tulajdonsagainak alapertekeit.
+	 * @param g: ezt allitja be a gameState attributumba.
+	 * @param c: ezen a cellan tartozkodik az ellenseg.
 	 */
 	public void init(GameState gameState, Cell cell){
 		Logger.enter("hobbit", "init", "gameState", "cell");
@@ -28,6 +31,11 @@ public class Hobbit extends Enemy {
 		Logger.exit("void");
 	}
 
+	/**
+	 * Az ellenseg leptetesenek metodusa.
+	 * Alapesetben ez az Enemy osztalyban valosul meg, de a szkeleton
+	 * mukodesenek lathatosaga miatt itt kellett megvalositani.
+	 */
 	public void step() {
 		Logger.enter("hobbit", "step", "", "");
 		
@@ -37,9 +45,16 @@ public class Hobbit extends Enemy {
 		neighbors[2].hasRoad();
 		neighbors[3].hasRoad();
 		neighbors[3].addEnemy(this);
+		formerCell.removeEnemy(this);
 		Logger.exit("void");
 	}
 	
+	/**
+	 * Vegrehajtja az ellenseg sebzeset.
+	 * Alapesetben ez az Enemy osztalyban valosul meg, de a szkeleton
+	 * mukodesenek lathatosaga miatt itt kellett megvalositani.
+	 * @param damageValue: a sebzes nagysaga.
+	 */
 	public void damage(int damageValue) {
 		Logger.enter("hobbit", "damageValue", "damageValue:int", "");
 		
