20661e9e721a533866564f9488cb53cba18b5d3e
ZsoltKovari <kovari.zsolt91@gmail.com>
2014-03-23 12:58:54 +0100

    Ellenőrzés+Komment

--- src/tedteam/twotowers/main/BlackStone.java
+++ src/tedteam/twotowers/main/BlackStone.java
@@ -1,12 +1,16 @@
 package tedteam.twotowers.main;
 
 import tedteam.twotowers.logger.Logger;
-
+/**
+ * A fekete varazskovet megvalosito osztaly.
+ * Ososztaly: MagicStone
+ */
 public class BlackStone extends MagicStone {
 
 	/**
-	 * 
-	 * @param b
+	 * Ez a metodus valtoztatja meg az akadaly lassitasanak merteket.
+	 * @param blocker: az akadaly, melyre a valtoztatast alkalmazni akarjuk.
+	 * @return a visszateresi ertek, hogy sikerult-e rarakni a kovet az akadalyra.
 	 */
 	public boolean effect(Blocker blocker) {
 		Logger.enter("blackStone", "effect", "blocker", "");
@@ -20,8 +24,10 @@ public class BlackStone extends MagicStone {
 	}
 
 	/**
-	 * 
-	 * @param t
+	 * Ez a metodus valtoztatja meg a torony lassitasanak merteket.
+	 * Mivel ilyen attributuma nincs a toronynak, ezert ez a metodus nem csinal semmit.
+	 * @param t: a torony, melyre a valtoztatast alkalmazni akarjuk.
+	 * @return a visszateresi ertek, hogy sikerult-e rarakni a kovet a toronyra (false).
 	 */
 	public boolean effect(Tower t){
 		return false;
