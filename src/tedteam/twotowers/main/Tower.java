01abcd8ef7e604eaaabb71a3753c11bf0a3d8248
ZsoltKovari <kovari.zsolt91@gmail.com>
2014-03-23 13:16:51 +0100

    Action átírása+ ékezetes komment javítás + Komment

--- src/tedteam/twotowers/main/Tower.java
+++ src/tedteam/twotowers/main/Tower.java
@@ -1,33 +1,66 @@
 package tedteam.twotowers.main;
 
 import tedteam.twotowers.logger.Logger;
-
+/**
+ * A tornyot megvalosito osztaly. Egy torony fobb
+ * tulajdonsagait tartalmazza.
+ * Ososztaly: Element
+ * Interface: IDamage
+ */
 public class Tower extends Element implements IDamage {
-
+	// A torp ellenseg sebzesenek merteke.
+	private int dwarfDamage;
+	
+	// A tunde ellenseg sebzesenek merteke.
+	private int elfDamage;
+	
+	// A hobbit ellenseg sebzesenek merteke.
+	private int hobbitDamage;
+	
+	// Az ember ellenseg sebzesenek merteke.
+	private int humanDamage;
+	
+	// A torony hatotavolsaga.
+	private int range;
+	
+	// A torony tuzelesi gyakorisaga.
+	private int speed;
+	
 	/**
-	 * 
-	 * @param m
+	 * Ez a metodus donti el, hogy a varazskovel lehet-e erositeni
+	 * a tornyot. Ha igen, akkor erositi is.
+	 * A szkeleton miatt itt konkret pelda.
+	 * @param greenStone: a varazsko, mellyel erositeni szeretnenk a tornyot.
+	 * @return visszajelzes, hogy sikeres volt-e a muvelet.
 	 */
 	public boolean enhance(MagicStone greenStone) {
-		Logger.enter("tower", "enchance", "greenStone", "");
+		Logger.enter("tower", "enhance", "greenStone", "");
 		greenStone.effect(this);
 		
 		Logger.exit("true");
 		return true;
 	}
 
-	
+	/**
+	 * A torony loveset vegrehajto metodus.
+	 */
 	public void action() {
 		Logger.enter("tower", "action", "", "");
-		
-		Cell cell1 = new Cell();
-		Enemy[] enemies = new Enemy[10];
-		enemies = cell1.getEnemy();
-		enemies[0].acceptDamage(this);
+		Cell[] cells=cell.getNeighbors();
+
+		cells[0].getEnemy();
+		cells[1].getEnemy();
+		Enemy hobbit=new Hobbit();
+		hobbit.acceptDamage(this);
+
 		
 		Logger.exit("void");	
 	}
 	
+	/**
+	 * Beallitja magat egy cellaba.
+	 * @param cell: ebbe a cellaba allitja be magat.
+	 */
 	public void setCell(Cell cell) {
 		Logger.enter("tower", "setCell", "cell", "");
 		
@@ -36,13 +69,20 @@ public class Tower extends Element implements IDamage {
 		Logger.exit("false");
 	}
 	
+	/**
+	 * Beallitja a torony lovesi gyakorisagat.
+	 * @param i: erre allitja be.
+	 */
 	public void setSpeed(int i) {
 		Logger.enter("tower", "setSpeed", "i:int", "");
 		
 		Logger.exit("true");		
 	}
 	
-
+	/**
+	 * A hobbit ellenseg sebzeseert felelos metodus.
+	 * @param e: az ellenseg melyet sebezni kell.
+	 */
 	@Override
 	public void affect(Hobbit e) {
 		Logger.enter("tower", "affect", "hobbit", "");
@@ -54,25 +94,40 @@ public class Tower extends Element implements IDamage {
 		
 	}
 
+	/**
+	 * A tunde ellenseg sebzeseert felelos metodus.
+	 * A szkeletonba nem szukseges megvalositani.
+	 * @param e: az ellenseg melyet sebezni kell.
+	 */
 	@Override
 	public void affect(Elf e) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * A torp ellenseg sebzeseert felelos metodus.
+	 * A szkeletonba nem szukseges megvalositani.
+	 * @param e: az ellenseg melyet sebezni kell.
+	 */
 	@Override
 	public void affect(Dwarf d) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * Az ember ellenseg sebzeseert felelos metodus.
+	 * A szkeletonba nem szukseges megvalositani.
+	 * @param e: az ellenseg melyet sebezni kell.
+	 */
 	@Override
 	public void affect(Human h) {
 		// TODO Auto-generated method stub	
 	}
 	
 	/**
-	 * Loggoláshoz, skeleton után törlendő!
+	 * Loggolashoz, skeleton utan torlendo!
 	 * @return
 	 */
 	public String loggerGetName() {
