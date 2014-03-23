28bcfc36f2a86a44ebbb155f539ffa0c1912b0c3
ZsoltKovari <kovari.zsolt91@gmail.com>
2014-03-23 13:13:39 +0100

    addStone visszatérési érték módosítás + Komment

--- src/tedteam/twotowers/main/Generator.java
+++ src/tedteam/twotowers/main/Generator.java
@@ -1,25 +1,39 @@
 package tedteam.twotowers.main; 
 
 import tedteam.twotowers.logger.Logger;
-
+/**
+ * Ez az osztaly felelos a felhasznalo altal jelzett muveletek
+ * vegrehajtasara, valamint az ellensegek letrehozasara.
+ */
 public class Generator {
-
+	// A palyan levo epitmenyeket tartalmazo osztaly.
 	private ElementList elementList = new ElementList();
+	
+	// A palyan levo ellensegeket tartalmazo osztaly.
 	private EnemyList enemyList = new EnemyList();
+	
+	// A jatekteret tartalmazo osztaly.
 	private GameField gameField = new GameField();
+	
+	// A jatekallapotot tartalmazo osztaly.
 	private GameState gameState = new GameState();
 
 	/**
-	 * 
-	 * @param m
-	 * @param c
+	 * Ez a metodus adja oda a cellan talalhato epitmenynek
+	 * a varazskovet.
+	 * @param m: az epitmenyre rakando varazsko.
+	 * @param c: a cella melyre a varazskovet akarjuk rakni.
+	 * @return sikeres volt-e a varazsko rarakasa az epitmenyre (false)
 	 */
 	public boolean addStone(MagicStone m, Cell c){
 		return false;
 	}
 
+	/**
+	 * Ez a metodus inicializalja a jatekteret.
+	 */
 	public void buildField(){
-		Logger.enter("gen", "buildField", "", "");
+		Logger.enter("generator", "buildField", "", "");
 		
 		gameField.init();
 		Cell cell4 = gameField.getFinalCell();
@@ -29,11 +43,14 @@ public class Generator {
 	}
 
 	/**
-	 * 
-	 * @param c
+	 * Ez a metodus felelos egy akadaly letrehozasaert
+	 * es cellara helyezeseert. Beallitja annak alapertekeit.
+	 * @param cell: erre a cellara akarjuk helyezni az akadalyt.
+	 * @return true ertekkel ter vissza ha az adott cellara
+	 * rahelyezheto az akadaly. false ha nem.
 	 */
 	public boolean createBlocker(Cell cell){
-		Logger.enter("gen", "createBlocker", "cell", "");
+		Logger.enter("generator", "createBlocker", "cell", "");
 		
 		cell.getElement();
 		Blocker blocker = new Blocker();
@@ -46,11 +63,14 @@ public class Generator {
 	}
 
 	/**
-	 * 
-	 * @param c
+	 * Ez a metodus felelos egy torony letrehozasaert
+	 * es cellara helyezeseert. Beallitja annak alapertekeit.
+	 * @param cell: erre a cellara akarjuk helyezni a tornyot.
+	 * @return true ertekkel ter vissza, ha az adott cellara
+	 * rahelyezheto a torony. false ha nem.
 	 */
 	public boolean createTower(Cell cell){
-		Logger.enter("gen", "createTower", "cell", "");
+		Logger.enter("generator", "createTower", "cell", "");
 		
 		cell.getElement();		
 		Tower tower = new Tower();
@@ -61,8 +81,13 @@ public class Generator {
 		return true;
 	}
 
+	/**
+	 * Ez a metodus generalja az uj ellensegeket a palyara.
+	 * Letrehozasa utan beallitja annak alapertekeit es
+	 * a palyara rakja.
+	 */
 	public void generateEnemies(){
-		Logger.enter("gen", "generateEnemies", "", "");
+		Logger.enter("generator", "generateEnemies", "", "");
 		Cell cell = gameField.getStartCell();
 		Hobbit hobbit = new Hobbit();
 		hobbit.init(gameState, cell);
@@ -71,22 +96,37 @@ public class Generator {
 		Logger.exit("void");
 	}
 	
-	public void addStone(BlackStone blackStone, Cell cell) {
-		Logger.enter("gen", "addStone", "blackStone", "cell");
+	/**
+	 * Ez a metodus adja oda a cellan talalhato epitmenynek
+	 * a varazskovet.
+	 * A szkeleton erthetosegenek celjabol itt konkret pelda talahato.
+	 * @param blackStone: a fekete varazsko melyet le akarunk rakni.
+	 * @param cell: a cella melyre a varazskovet akarjuk rakni.
+	 */
+	public boolean addStone(BlackStone blackStone, Cell cell) {
+		Logger.enter("generator", "addStone", "blackStone", "cell");
 		
 		Blocker blocker = (Blocker)cell.getElement();
 		blocker.enhance(blackStone);
 		
-		Logger.exit("void");
+		Logger.exit("true");
+		return true;
 	}
-	
-	public void addStone(GreenStone greenStone, Cell cell) {
-		Logger.enter("gen", "addStone", "greenStone", "cell");
+	/**
+	 * Ez a metodus adja oda a cellan talalhato epitmenynek
+	 * a varazskovet.
+	 * A szkeleton erthetosegenek celjabol itt konkret pelda talalhato.
+	 * @param greenStone: a  zold varazsko melyet le akarunk rakni.
+	 * @param cell: a cella melyre a varazskovet akarjuk rakni.
+	 */
+	public boolean addStone(GreenStone greenStone, Cell cell) {
+		Logger.enter("generator", "addStone", "greenStone", "cell");
 		
 		Tower tower = (Tower)cell.getElement();
 		tower.enhance(greenStone);
 		
-		Logger.exit("void");
+		Logger.exit("true");
+		return true;
 	}
 
 }
