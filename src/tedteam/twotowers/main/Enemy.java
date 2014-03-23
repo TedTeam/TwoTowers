fac59b97577df23b2d029f43caaf1949a6ebc50b
ZsoltKovari <kovari.zsolt91@gmail.com>
2014-03-23 13:08:58 +0100

    FormerCell modositas + Komment

--- src/tedteam/twotowers/main/Enemy.java
+++ src/tedteam/twotowers/main/Enemy.java
@@ -1,45 +1,63 @@
 package tedteam.twotowers.main;
+/**
+ * Az ellensegeket megvalosito absztrakt osztaly.
+ */
 
 public abstract class Enemy {
-
+	// Az ellenseg aktualis tartozkodasi helye.
 	protected Cell cell = new Cell();
+	
+	// Az ellenseg aktualis eletereje.
 	private int currentLifePoint;
-	private Cell formerCell;
+	
+	// Az ellenseg elozo tartozkodasi helye.
+	protected Cell formerCell=new Cell();
+	
+	// Ezen a GameState referencian keresztul tudja magat
+	// leelenorizni az elleseg, hogy meghalt-e,
+	// es tudja magat torolni az ellenség listabol
 	private GameState gameState;
+	
+	// Az ellenseg eredeti eletereje.
 	private int maxLifePoint;
+	
+	// Az ellenseg sebessege.
 	private int speed;
 
 	/**
-	 * 
-	 * @param d
+	 * Ezen a metoduson keresztul valtozik az ellenseg eletereje.
+	 * @param d: a valtozas merteke.
 	 */
 	public abstract void acceptDamage(IDamage d);
 
 	/**
-	 * 
-	 * @param s
+	 * Vegrehajtja az ellenseg blokkolasat.
+	 * @param slowing: ilyen mertekben lassitja le az ellenseget.
 	 */
 	public void block(int slowing){
 
 	}
 
 	/**
-	 * 
-	 * @param dv
+	 * Vegrehajtja az ellenseg sebzeset.
+	 * @param damageValue: a sebzes nagysaga.
 	 */
 	public void damage(int damageValue){
-
+		
 	}
 
 	/**
-	 * 
-	 * @param g
-	 * @param c
+	 * Beallitja az ellenseg tulajdonsagainak alapertekeit.
+	 * @param g: ezt allitja be a gameState attributumba.
+	 * @param c: ezen a cellan tartozkodik az ellenseg.
 	 */
 	public abstract void init(GameState g, Cell c);
 
+	/**
+	 * Az ellenseg leptetesenek metodusa.
+	 */
 	public void step(){
-
+		
 	}
 
 }
