9b8a0763ff2b0fcf1a8add94b9eca56909649536
ZsoltKovari <kovari.zsolt91@gmail.com>
2014-03-23 13:11:20 +0100

    addEnemy logger param. módosítás hobbitra + Komment

--- src/tedteam/twotowers/main/EnemyList.java
+++ src/tedteam/twotowers/main/EnemyList.java
@@ -1,30 +1,34 @@
 package tedteam.twotowers.main;
 
 import tedteam.twotowers.logger.Logger;
-
+/**
+ * A palyan levo ellensegeket tarolo osztaly.
+ */
 public class EnemyList {
-
+	// A palyan levo ellensegek tombje.
 	private Enemy enemies[] = new Enemy[20];
 
 	/**
-	 * 
-	 * @param e
+	 * Hozzaad egy ellenseget az enemies tombhoz.
+	 * @param enemy: a hozzaadando ellenseg.
 	 */
 	public void addEnemy(Enemy enemy){
-		Logger.enter("enemyList", "addEnemy", "enemy", "");
+		Logger.enter("enemyList", "addEnemy", "hobbit", "");
 		
 		Logger.exit("void");
 	}
 
 	/**
-	 * 
-	 * @param e
+	 * Torol egy ellenseget az enemies tombbol.
+	 * @param e: ezt torli a tombbol.
 	 */
 	public void deleteEnemy(Enemy e){
 
 	}
 
-
+	/**
+	 * Meghivja az osszes tartalmazott ellenseg lepteto metodusat.
+	 */
 	public void stepAll(){
 		Logger.enter("enemyList", "stepAll", "","");
 		
@@ -33,6 +37,10 @@ public class EnemyList {
 		Logger.exit("void");
 	}
 
+	/**
+	 * Meghivja az osszes tartalmazott ellenseg acceptDamage metodusat.
+	 * @param d: ezt adja parameterul az acceptDamage metodusoknak.
+	 */
 	public void notifyAllEnemy(IDamage d) {
 		
 	}
